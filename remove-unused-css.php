075dab13bd2cff9681ea96cdadac141d8e9503c5
Евгений <40506054+commeta@users.noreply.github.com>
2024-10-22 23:11:52 +0300

    Update remove-unused-css.php

--- remove-unused-css.php
+++ remove-unused-css.php
@@ -7,146 +7,239 @@
  * 
  */
 
+// Подключаем autoload для sabberworm/php-css-parser
+require_once 'vendor/autoload.php';
 
-// Файл для хранения общего массива неиспользуемых правил
-$storageFile = __DIR__ . '/unused_rules.json';
+use Sabberworm\CSS\Parser;
+use Sabberworm\CSS\CSSList\Document;
+use Sabberworm\CSS\Value\URL;
 
-// Получаем данные из POST-запроса
-$data = file_get_contents('php://input');
+/**
+ * Главный скрипт для приема данных неиспользуемых CSS-селекторов
+ * Сохраняет общий массив неиспользуемых правил и создает очищенные CSS-файлы
+ */
+
+// Файл для хранения глобального массива селекторов
+define('SELECTORS_FILE', 'data/unused_selectors.ser');
+
+// Каталог для сохранения очищенных CSS-файлов
+define('CLEAN_CSS_DIR', dirname(__DIR__) . '/clean-css/');
+
+// Получаем данные POST
+$inputJSON = file_get_contents('php://input');
+$data = json_decode($inputJSON, true);
+
 if (!$data) {
     http_response_code(400);
-    exit('Нет данных для обработки.');
+    echo 'Invalid JSON data';
+    exit;
 }
 
-$receivedData = json_decode($data, true);
-if (json_last_error() !== JSON_ERROR_NONE) {
-    http_response_code(400);
-    exit('Неверный формат JSON.');
-}
-
-// Загружаем общий массив неиспользуемых правил
-if (file_exists($storageFile)) {
-    $unusedRules = json_decode(file_get_contents($storageFile), true);
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        $unusedRules = [];
-    }
+// Проверяем наличие файла с селекторами или инициализируем массив
+if (file_exists(SELECTORS_FILE)) {
+    $masterSelectors = unserialize(file_get_contents(SELECTORS_FILE));
 } else {
-    $unusedRules = [];
+    $masterSelectors = [];
+    
+    initializeMasterSelectors($masterSelectors);
+    file_put_contents(SELECTORS_FILE, serialize($masterSelectors));
 }
 
-// Функция для преобразования относительных путей в абсолютные
-function resolvePath($basePath, $relativePath) {
-    if (parse_url($relativePath, PHP_URL_SCHEME) != '') {
-        // Это абсолютный URL
-        return $relativePath;
+// Обновляем статус селекторов на основе полученных данных
+updateSelectorsUsage($masterSelectors, $data);
+
+// Сохраняем обновленные селекторы
+file_put_contents(SELECTORS_FILE, serialize($masterSelectors));
+
+// Создаем очищенные CSS-файлы
+cleanCssFiles($masterSelectors);
+
+echo 'Данные успешно обработаны';
+
+/**
+ * Инициализирует массив селекторов, парсит все CSS-файлы на сервере
+ */
+function initializeMasterSelectors(&$masterSelectors){
+    $cssFiles = findCssFiles($_SERVER['DOCUMENT_ROOT']);
+    
+    foreach ($cssFiles as $filePath) {
+        $relativePath = getRelativePath($filePath);
+        parseCssFile($filePath, $relativePath, $masterSelectors);
     }
-    // Нормализуем пути
-    $basePath = rtrim(str_replace('\\', '/', $basePath), '/') . '/';
-    $relativePath = str_replace('\\', '/', $relativePath);
-    $path = $basePath . $relativePath;
-    $parts = array_filter(explode('/', $path), 'strlen');
-    $absolutes = [];
-    foreach ($parts as $part) {
-        if ('.' == $part) continue;
-        if ('..' == $part) {
-            array_pop($absolutes);
-        } else {
-            $absolutes[] = $part;
-        }
-    }
-    return '/' . implode('/', $absolutes);
 }
 
-// Обрабатываем полученные данные
-foreach ($receivedData as $sheetData) {
-    $stylesheetPath = $sheetData['stylesheet'];
-    // Преобразуем относительный путь в абсолютный
-    $stylesheetPath = resolvePath($_SERVER['DOCUMENT_ROOT'], $stylesheetPath);
-
-    // Проверяем, что файл стиля находится на сервере
-    if (strpos($stylesheetPath, $_SERVER['DOCUMENT_ROOT']) !== 0) {
-        continue; // Игнорируем внешние стили
-    }
-
-    // Инициализируем массив правил для данного файла, если его еще нет
-    if (!isset($unusedRules[$stylesheetPath])) {
-        $unusedRules[$stylesheetPath] = [];
-    }
-
-    foreach ($sheetData['rules'] as $rule) {
-        $ruleKey = md5($rule['cssText']);
-
-        // Если правило уже есть в общем массиве, пропускаем
-        if (isset($unusedRules[$stylesheetPath][$ruleKey])) {
+/**
+ * Обновляет статус селекторов на основе полученных данных от страниц
+ */
+function updateSelectorsUsage(&$masterSelectors, $data){
+    foreach ($data as $href => $selectors) {
+        $relativePath = getFilePathFromHref($href);
+        
+        if (!isset($masterSelectors[$relativePath])) {
             continue;
         }
-
-        // Добавляем правило в общий массив неиспользуемых правил
-        $unusedRules[$stylesheetPath][$ruleKey] = $rule['cssText'];
-    }
-}
-
-// Сохраняем обновленный общий массив неиспользуемых правил
-file_put_contents($storageFile, json_encode($unusedRules));
-
-// Функция для очистки CSS файлов от неиспользуемых правил
-function cleanCssFiles($unusedRules) {
-    $cleanCssDir = __DIR__ . '/clean-css';
-    if (!is_dir($cleanCssDir)) {
-        mkdir($cleanCssDir, 0777, true);
-    }
-
-    foreach ($unusedRules as $stylesheetPath => $rules) {
-        // Проверяем, что оригинальный файл существует
-        if (!file_exists($stylesheetPath)) {
-            continue;
-        }
-
-        // Содержимое оригинального файла
-        $cssContent = file_get_contents($stylesheetPath);
-
-        // Парсинг CSS с помощью CssParser
-        require_once 'vendor/autoload.php';
-        $parser = new Sabberworm\CSS\Parser($cssContent);
-        $cssDocument = $parser->parse();
-
-        // Удаляем неиспользуемые правила
-        foreach ($cssDocument->getAllDeclarationBlocks() as $block) {
-            $cssText = $block->render();
-            $ruleKey = md5($cssText);
-            if (isset($rules[$ruleKey])) {
-                $cssDocument->remove($block);
+        
+        $unusedSelectors = array_column($selectors, 'selector');
+        
+        foreach ($masterSelectors[$relativePath] as $selector => &$status) {
+            if (!in_array($selector, $unusedSelectors)) {
+                $status = 'used';
             }
         }
+    }
+}
 
-        // Удаляем неиспользуемые @media, @font-face, @keyframes и т.д.
-        foreach ($cssDocument->getContents() as $content) {
-            if ($content instanceof Sabberworm\CSS\CSSList\AtRuleBlockList) {
-                $cssText = $content->render();
-                $ruleKey = md5($cssText);
-                if (isset($rules[$ruleKey])) {
-                    $cssDocument->remove($content);
+/**
+ * Создает очищенные CSS-файлы, вырезая неиспользуемые правила
+ */
+function cleanCssFiles($masterSelectors){
+    foreach ($masterSelectors as $relativePath => $selectors) {
+        $filePath = $_SERVER['DOCUMENT_ROOT'] . $relativePath;
+        
+        if (!file_exists($filePath)) {
+            continue;
+        }
+        
+        $cssContent = file_get_contents($filePath);
+        $parser = new Parser($cssContent);
+        $cssDoc = $parser->parse();
+        
+        removeUnusedRules($cssDoc, $selectors, dirname($filePath));
+        
+        $cleanCssPath = CLEAN_CSS_DIR . $relativePath;
+        
+        ensureDirectoryExists(dirname($cleanCssPath));
+        
+        file_put_contents($cleanCssPath, $cssDoc->render());
+    }
+}
+
+/**
+ * Рекурсивно удаляет неиспользуемые правила из CSS-документа
+ */
+function removeUnusedRules(Document $cssDoc, $selectorsStatus, $baseDir){
+    foreach ($cssDoc->getContents() as $key => $rule) {
+        if ($rule instanceof Sabberworm\CSS\RuleSet\DeclarationBlock) {
+            $selectorObjects = $rule->getSelectors();
+            $unused = true;
+            
+            foreach ($selectorObjects as $selectorObj) {
+                $selector = (string)$selectorObj;
+                
+                if (isset($selectorsStatus[$selector]) && $selectorsStatus[$selector] === 'used') {
+                    $unused = false;
+                    break;
                 }
             }
+            
+            if ($unused) {
+                $cssDoc->remove($rule);
+            }
+        } elseif ($rule instanceof Sabberworm\CSS\CSSList\AtRuleBlock) {
+            removeUnusedRules($rule, $selectorsStatus, $baseDir);
+            
+            if (empty($rule->getContents())) {
+                $cssDoc->remove($rule);
+            }
+        } elseif ($rule instanceof Sabberworm\CSS\Property\AtRule) {
+            // Обрабатываем @import
+            // ...
         }
-
-        // Путь для сохранения очищенного файла
-        $relativePath = str_replace($_SERVER['DOCUMENT_ROOT'], '', $stylesheetPath);
-        $cleanFilePath = $cleanCssDir . $relativePath;
-
-        // Создаем нужные директории
-        $cleanFileDir = dirname($cleanFilePath);
-        if (!is_dir($cleanFileDir)) {
-            mkdir($cleanFileDir, 0777, true);
-        }
-
-        // Сохраняем очищенный CSS
-        file_put_contents($cleanFilePath, $cssDocument->render());
     }
 }
 
-// Очищаем CSS файлы от неиспользуемых правил
-cleanCssFiles($unusedRules);
+/**
+ * Парсит CSS-файл и добавляет селекторы в masterSelectors
+ */
+function parseCssFile($filePath, $relativePath, &$masterSelectors){
+    $cssContent = file_get_contents($filePath);
+    $parser = new Parser($cssContent);
+    $cssDoc = $parser->parse();
 
-// Отправляем ответ клиенту
-echo 'Неиспользуемый CSS успешно обработан.';
+    $selectors = [];
+
+    collectSelectors($cssDoc, $selectors);
+
+    foreach ($selectors as $selector) {
+        $masterSelectors[$relativePath][$selector] = $masterSelectors[$relativePath][$selector] ?? 'unused';
+    }
+}
+
+/**
+ * Собирает селекторы из CSS-документа
+ */
+function collectSelectors(Document $cssDoc, &$selectors){
+    foreach ($cssDoc->getContents() as $rule) {
+        if ($rule instanceof Sabberworm\CSS\RuleSet\DeclarationBlock) {
+            $selectorObjects = $rule->getSelectors();
+            
+            foreach ($selectorObjects as $selectorObj) {
+                $selectors[] = (string)$selectorObj;
+            }
+        } elseif ($rule instanceof Sabberworm\CSS\CSSList\AtRuleBlock) {
+            collectSelectors($rule, $selectors);
+        }
+    }
+}
+
+/**
+ * Ищет все CSS-файлы в указанном каталоге
+ */
+function findCssFiles($dir){
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+    $cssFiles = [];
+    
+    foreach ($iterator as $file) {
+        if ($file->isFile() && strtolower($file->getExtension()) === 'css') {
+            $cssFiles[] = $file->getPathname();
+        }
+    }
+    
+    return $cssFiles;
+}
+
+/**
+ * Получает относительный путь файла от корня документа
+ */
+function getRelativePath($filePath){
+    return str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath($filePath));
+}
+
+/**
+ * Преобразует href в путь файла на сервере
+ */
+function getFilePathFromHref($href){
+    $urlComponents = parse_url($href);
+    $path = $urlComponents['path'] ?? '';
+    
+    return $path;
+}
+
+/**
+ * Обеспечивает существование каталога
+ */
+function ensureDirectoryExists($directory){
+    if (!file_exists($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
+
+/**
+ * Парсит URL из @import правила
+ */
+function parseImportUrl($args, $baseDir){
+    $url = trim($args, ' \'\"');
+    
+    if (strpos($url, 'url(') === 0) {
+        $url = substr($url, 4, -1);
+    }
+    
+    if (!preg_match('#^https?://#', $url)) {
+        $importPath = realpath($baseDir . '/' . $url);
+        if ($importPath && file_exists($importPath)) {
+            return $importPath;
+        }
+    }
+    
+    return null;
+}
